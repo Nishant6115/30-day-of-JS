aec2be4ce12f3610a482e6332ad079715db6133e
Nishant6115 <nishntsharma210106@gmail.com>
2024-07-19 17:53:43 +0530

    updating the day1 folder

---
+++ day1.js
@@ -0,0 +1,9 @@
+// Activity 1: Variable Declaration
+
+// Task 1: Declare a variable using `var`, assign it a number, and log the value to the console.
+var num = 2;
+console.log(num); // Output: 2
+
+// Task 2: Declare a variable using `let`, assign it a string, and log the value to the console.
+let sentence = "Learing javaScript...";
+console.log(sentence); // Output: Learing javaScript...
